37fd904af23e24837074b977f57f70c9704d0e70
Shahnawaz <shahnawazkhan333@live.com>
2024-02-21 15:07:03 +0500

    Added my project

---
+++ backend/seedData.js
@@ -0,0 +1,105 @@
+module.exports = [
+	{
+		companyName: 'Toyota',
+		distanceCovered: 10000,
+		mileage: 25,
+		serviceDates: [new Date('2022-01-15'), new Date('2022-03-20')],
+		owner: {
+			name: 'John Doe',
+			email: 'john.doe@example.com',
+		},
+		image: 'https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	},
+	{
+		companyName: 'Honda',
+		distanceCovered: 8000,
+		mileage: 28,
+		serviceDates: [new Date('2022-02-10'), new Date('2022-04-25')],
+		owner: {
+			name: 'Jane Smith',
+			email: 'jane.smith@example.com',
+		},
+		image: 'https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	},
+
+	{
+		companyName: 'Volkswagen',
+		distanceCovered: 10500,
+		mileage: 26,
+		serviceDates: [new Date('2022-10-05'), new Date('2022-12-15')],
+		owner: {
+			name: 'Ava Anderson',
+			email: 'ava.anderson@example.com',
+		},
+		image: 'https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	},
+	{
+		companyName: 'Toyota',
+		distanceCovered: 10000,
+		mileage: 25,
+		serviceDates: [new Date('2022-01-15'), new Date('2022-03-20')],
+		owner: {
+			name: 'John Doe',
+			email: 'john.doe@example.com',
+		},
+		image: 'https://images.pexels.com/photos/116675/pexels-photo-116675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	},
+	{
+		companyName: 'Honda',
+		distanceCovered: 8000,
+		mileage: 28,
+		serviceDates: [new Date('2022-02-10'), new Date('2022-04-25')],
+		owner: {
+			name: 'Jane Smith',
+			email: 'jane.smith@example.com',
+		},
+		image: 'https://images.pexels.com/photos/707046/pexels-photo-707046.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	},
+
+	{
+		companyName: 'Volkswagen',
+		distanceCovered: 10500,
+		mileage: 26,
+		serviceDates: [new Date('2022-10-05'), new Date('2022-12-15')],
+		owner: {
+			name: 'Ava Anderson',
+			email: 'ava.anderson@example.com',
+		},
+		image: 'https://images.pexels.com/photos/909907/pexels-photo-909907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	},
+	{
+		companyName: 'Toyota',
+		distanceCovered: 10000,
+		mileage: 25,
+		serviceDates: [new Date('2022-01-15'), new Date('2022-03-20')],
+		owner: {
+			name: 'John Doe',
+			email: 'john.doe@example.com',
+		},
+		image: 'https://images.pexels.com/photos/733745/pexels-photo-733745.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	},
+	{
+		companyName: 'Honda',
+		distanceCovered: 8000,
+		mileage: 28,
+		serviceDates: [new Date('2022-02-10'), new Date('2022-04-25')],
+		owner: {
+			name: 'Jane Smith',
+			email: 'jane.smith@example.com',
+		},
+		image: 'https://images.pexels.com/photos/244206/pexels-photo-244206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	},
+
+	{
+		companyName: 'Volkswagen',
+		distanceCovered: 10500,
+		mileage: 26,
+		serviceDates: [new Date('2022-10-05'), new Date('2022-12-15')],
+		owner: {
+			name: 'Ava Anderson',
+			email: 'ava.anderson@example.com',
+		},
+		image: 'https://images.pexels.com/photos/810357/pexels-photo-810357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	},
+
+];
